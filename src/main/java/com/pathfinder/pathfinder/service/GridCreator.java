1f0b6de2b46a89b0445ed44160f21e0c73fd9c98
Marcin Gwóźdź <marcin.gwozdz@testto.com>
2024-04-17 16:38:49 +0200

    Refactor GridCreator method in Pathfinder service
    Modified the createGrid method in the GridCreator class to improve readability and clarity. Instead of directly using the 'rows+2' and 'columns+2' in the array initialization, they are assigned to variables 'width' and 'height'. These variables are then used to define and manipulate the grid size.

--- src/main/java/com/pathfinder/pathfinder/service/GridCreator.java
+++ src/main/java/com/pathfinder/pathfinder/service/GridCreator.java
@@ -7,14 +7,16 @@ import org.springframework.stereotype.Service;
 public class GridCreator {
 
     public int[][] createGrid(int rows, int columns) {
-        int[][] grid = new int[rows+2][columns+2];
+        int width = rows + 2;
+        int height = columns + 2;
+        int[][] grid = new int[width][height];
         for(int i = 0; i < grid[0].length; i++){
             grid[0][i] = ObjectType.WALL.value;
-            grid[grid[0].length - 1][i] = ObjectType.WALL.value;
+            grid[width - 1][i] = ObjectType.WALL.value;
         }
         for(int i = 0; i < grid.length; i++){
             grid[i][0] = ObjectType.WALL.value;
-            grid[i][grid.length - 1] = ObjectType.WALL.value;
+            grid[i][height - 1] = ObjectType.WALL.value;
         }
         return grid;
     }
